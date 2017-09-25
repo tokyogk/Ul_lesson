4193949ad9c48188c02003d8969804dce88ebc67
tokyogk <tokyo.gk5@gmail.com>
2017-09-25 13:18:29 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/gou/kuwabara/ul_lesson/MainActivity.java
@@ -0,0 +1,28 @@
+package jp.techacademy.gou.kuwabara.ul_lesson;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("UI_PARTS", "ボタンをタップしました");
+            }
+        });
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("テキスト");
+    }
+}
